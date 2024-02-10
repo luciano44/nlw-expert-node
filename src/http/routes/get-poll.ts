00d3da023785e3b46f188a788073f94851286aa7
Luciano Ribeiro <luciano.ar6@gmail.com>
2024-02-09 22:52:52 -0300

    add each vote to redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import prisma from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -24,6 +25,34 @@ export async function getPoll(app: FastifyInstance) {
       },
     })
 
-    return reply.send({ poll })
+    if (!poll) {
+      return reply.status(400).send({ msg: "Poll not found" })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES")
+
+    const votes: Record<string, number> = {}
+
+    // Convert result votes string into object voteOptionId:voteCount format
+    for (let i = 0; i < result.length; i = i + 2) {
+      const voteId = result[i]
+      const voteCount = parseInt(result[i + 1])
+
+      votes[voteId] = voteCount
+    }
+
+    console.log(votes)
+
+    return reply.send({
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map((option) => {
+        return {
+          id: option.id,
+          title: option.title,
+          score: option.id in votes ? votes[option.id] : 0,
+        }
+      }),
+    })
   })
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import { randomUUID } from "node:crypto"
 import { z } from "zod"
 import prisma from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -39,6 +40,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({ msg: "You already voted on this poll" })
       }
@@ -62,6 +65,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send()
   })
